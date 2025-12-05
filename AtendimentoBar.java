6ea2d4c9f9346413f9d7d53092519a5997ece2b2
Matheus Castilho <matheusc.pavam@gmail.com>
2025-12-05 13:53:22 -0300

    Remove .gitignore and update images

--- AtendimentoBar.java
+++ AtendimentoBar.java
@@ -1,6 +1,3 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
